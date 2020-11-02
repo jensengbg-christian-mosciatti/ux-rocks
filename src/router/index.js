ba74f4e6a328e5ef25ec8b86ef1031e6e7c07556
jensengbg-christian-mosciatti <christian.mosciatti@gmail.com>
2020-11-02 12:04:11 +0100

    added routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,26 +1,50 @@
-import { createRouter, createWebHashHistory } from "vue-router";
-import Home from "../views/Home.vue";
+import { createRouter, createWebHashHistory } from 'vue-router'
+import Home from '../views/Home.vue'
+import Lineup from '../views/Lineup.vue'
+import Contact from '../views/Contact.vue'
+import Faq from '../views/Faq.vue'
+import Tickets from '../views/tickets.vue'
 
 const routes = [
   {
-    path: "/",
-    name: "Home",
-    component: Home
+    path: '/',
+    name: 'Home',
+    component: Home,
   },
   {
-    path: "/about",
-    name: "About",
+    path: '/lineup',
+    name: 'Lineup',
+    component: Lineup,
+  },
+  {
+    path: '/contact',
+    name: 'Contact',
+    component: Contact,
+  },
+  {
+    path: '/faq',
+    name: 'Faq',
+    component: Faq,
+  },
+  {
+    path: '/tickets',
+    name: 'Tickets',
+    component: Tickets,
+  },
+  {
+    path: '/about',
+    name: 'About',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
-  }
-];
+      import(/* webpackChunkName: "about" */ '../views/About.vue'),
+  },
+]
 
 const router = createRouter({
   history: createWebHashHistory(),
-  routes
-});
+  routes,
+})
 
-export default router;
+export default router
