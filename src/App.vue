0e9aca972582d4e0b98d78107e2870a64eed220c
jenseneducation-adin-ehnsio <55272975+jenseneducation-adin-ehnsio@users.noreply.github.com>
2020-11-02 16:11:56 +0100

    buttons styled

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,7 @@
 </template>
 
 <style lang="scss">
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -25,6 +26,18 @@
     &.router-link-exact-active {
       color: #42b983;
     }
+  }
+}
+
+button {
+  cursor: pointer;
+  border: none;
+
+  &:focus {
+    outline: none;
+  }
+  &:disabled {
+    box-shadow: none;
   }
 }
 </style>
