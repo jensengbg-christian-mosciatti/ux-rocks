b96aefa71b1d9410345ddf991d1305b07811a39d
Frida Johansson <frida_mikaela@hotmail.com>
2020-11-03 19:31:37 +0100

    mobile nav

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,44 @@
 <template>
   <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/">
+      <img src="./assets/uxrocks.svg" alt="logo" />
+    </router-link>
+
+    <img @click="showModal = true" src="./assets/menu.svg" alt="menu" />
+
+    <MenuModal v-if="showModal" @close="showModal = false">
+      <h1>hej</h1>
+    </MenuModal>
   </div>
   <router-view />
 </template>
+
+<script>
+import MenuModal from '@/components/menuModal.vue'
+export default {
+  data() {
+    return {
+      showModal: false,
+    }
+  },
+  components: {
+    MenuModal,
+  },
+  // methods: {
+  //   showMenuModal() {
+  //     this.showModal = true
+  //   },
+  //   closeMenuModal() {
+  //     this.showModal = false
+  //   },
+  // },
+  // data() {
+  //   return {
+  //     showModal: false,
+  //   }
+  // },
+}
+</script>
 
 <style lang="scss">
 * {
@@ -25,20 +59,23 @@
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  // color: #2c3e50;
 }
 
 #nav {
-  padding: 30px;
+  margin: $margin-v-img;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
+  // a {
+  //   font-weight: bold;
+  //   color: #2c3e50;
 
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+  //   &.router-link-exact-active {
+  //     color: #42b983;
+  //   }
+  // }
 }
 
 button {
