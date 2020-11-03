115d764a342cd9380098c281789fe9223258cfa5
jensengbg-christian-mosciatti <christian.mosciatti@gmail.com>
2020-11-03 21:04:26 +0100

    started design of TicketShop.vue

---
+++ src/components/TicketShop.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <div>
+      <label for="name">Name</label>
+      <input type="text" name="name" id="name" />
+    </div>
+    <div>
+      <label for="email">Email</label>
+      <input type="text" name="email" id="email" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped>
+label {
+  color: $white;
+}
+</style>
