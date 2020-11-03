fa7d2f3e849d2fcb5dace03928b569b1485b39a3
jenseneducation-Clara-Berglund <54704837+jenseneducation-Clara-Berglund@users.noreply.github.com>
2020-11-03 20:15:04 +0100

    Add InputField Component

---
+++ src/components/InputField.vue
@@ -0,0 +1,88 @@
+<template>
+  <div class="inputContainer">
+    <p v-if="disabled" class="p-disabled">{{ label }}</p>
+    <p v-if="!disabled">{{ label }}</p>
+    <input v-if="!multiline" type="text" size="50" :disabled="disabled" />
+    <textarea v-if="multiline" size="50" rows="6" :disabled="disabled" />
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    label: String,
+    multiline: Boolean,
+    disabled: Boolean,
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.inputContainer {
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  margin-top: $margin-v-inputfield;
+  margin-left: $margin-h-inputfield;
+  margin-right: $margin-h-inputfield;
+}
+
+p {
+  color: $white;
+  font-family: $font-input;
+}
+
+.p-disabled {
+  color: $dark-grey;
+}
+
+input {
+  background-color: $dark-grey;
+  height: $input-height;
+  border-radius: $input-br;
+  border: none;
+  box-shadow: $drop-shadow;
+  color: $white;
+  font-family: $font-input;
+  outline-style: none;
+  text-indent: 2.5em;
+  width: 100%;
+
+  &:focus {
+    box-shadow: $focus-glow-input;
+  }
+
+  &:disabled {
+    background-color: $black;
+    color: $dark-grey;
+    box-shadow: none;
+    border-color: $dark-grey;
+    cursor: auto;
+  }
+}
+
+textarea {
+  background-color: $dark-grey;
+  border-radius: $input-br;
+  border: none;
+  box-shadow: $drop-shadow;
+  color: $white;
+  font-family: $font-input;
+  outline-style: none;
+  text-indent: 2.5em;
+  width: 100%;
+  padding-top: 1em;
+
+  &:focus {
+    box-shadow: $focus-glow-input;
+  }
+
+  &:disabled {
+    background-color: $black;
+    color: $dark-grey;
+    box-shadow: none;
+    border-color: $dark-grey;
+    cursor: auto;
+  }
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,23 +2,32 @@
   <div class="home">
     <PrimaryBtn title="PRIMARY" />
     <GhostBtn title="GHOST" />
+
+    <!-- En container så man ser active-effekten (som är vit)-->
+    <div style="background-color: black; padding: 1.5em;">
+      <InputField label="Name" />
+      <InputField label="Name" disabled />
+      <InputField label="Message" multiline />
+      <InputField label="Message" multiline disabled />
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import PrimaryBtn from "@/components/PrimaryBtn";
-import GhostBtn from "@/components/GhostBtn";
+import PrimaryBtn from '@/components/PrimaryBtn'
+import GhostBtn from '@/components/GhostBtn'
+import InputField from '@/components/InputField'
 
 export default {
-  
-  name: "Home",
+  name: 'Home',
   components: {
-    PrimaryBtn, GhostBtn
-  }
-};
+    PrimaryBtn,
+    GhostBtn,
+    InputField,
+  },
+}
 </script>
 
 <style lang="scss" scoped>
-
 </style>
