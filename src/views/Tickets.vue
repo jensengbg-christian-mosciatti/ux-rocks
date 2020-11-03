1ab3b8babef75d5645df9556b723e59e5527089d
jensengbg-christian-mosciatti <christian.mosciatti@gmail.com>
2020-11-03 08:42:06 +0100

    added fonts, some general styles in _variables.scss file, and a style view

--- src/views/Tickets.vue
+++ src/views/Tickets.vue
@@ -1,7 +1,30 @@
-<template> <div></div> </template>
+<template>
+  <div class="cont"></div>
+</template>
 
 <script>
 export default {}
 </script>
 
-<style></style>
+<style lang="scss" scoped>
+.cont div {
+  margin: 1em;
+  width: 400px;
+  // height: 100px;
+  background-color: rgb(26, 28, 32);
+  color: radial-gradient(
+    circle,
+    rgba(26, 28, 32, 0.33) 20%,
+    rgba(26, 28, 32, 0.75) 100%
+  );
+}
+h1,
+h3,
+h2 {
+  color: $gold;
+}
+p {
+  color: $white;
+  padding: $margin-v-p 1em;
+}
+</style>
