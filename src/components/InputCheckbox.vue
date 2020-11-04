b2ce5c34cbd1303c265f188cdb6f01aad764bb08
jensengbg-christian-mosciatti <christian.mosciatti@gmail.com>
2020-11-04 22:19:57 +0100

    Finished input checkbox, small adjustments on _variables

---
+++ src/components/InputCheckbox.vue
@@ -0,0 +1,113 @@
+<template>
+  <div class="inputContainer">
+    <input :disabled="disabled" type="checkbox" :name="label" :id="label" />
+    <label :for="label"
+      >{{ label }}<span class="checkmark">&#8515;</span></label
+    >
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    label: String,
+    disabled: Boolean,
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.inputContainer {
+  display: inline-flex;
+  flex-direction: row-reverse;
+  justify-content: flex-end;
+  align-items: center;
+  margin-top: $margin-v-inputfield;
+  // margin-left: $margin-h-inputfield;
+  // margin-right: $margin-h-inputfield;
+}
+
+label {
+  position: relative;
+  color: $white;
+  font-family: $font-input;
+}
+
+.checkmark {
+  visibility: hidden;
+  position: absolute;
+  right: -0.8em;
+  top: -0.2em;
+  width: 0;
+  height: 0;
+  user-select: none;
+
+  font-family: $font-input;
+  font-size: 2em;
+  line-height: 0.4em;
+  color: $white;
+  text-shadow: 0 0 0.3em $white;
+  transform: rotate(45deg);
+}
+
+input {
+  margin-left: 1em;
+  width: 1.57em;
+  height: 1.57em;
+  appearance: none;
+  outline: none;
+  background-color: $dark-grey;
+
+  border-radius: 0.2em;
+  border: 2px solid $dark-grey;
+
+  box-shadow: $drop-shadow;
+  color: $white;
+
+  &:focus {
+    box-shadow: $focus-glow-input;
+    border: 2px solid $white;
+  }
+  &:focus + label {
+    text-shadow: 0 0 0.2em $white;
+  }
+  &:checked ~ label .checkmark {
+    visibility: visible;
+  }
+
+  &:disabled {
+    background-color: $black;
+    // border: 2px solid $dark-grey;
+  }
+  &:disabled + label {
+    color: $dark-grey;
+  }
+}
+// input {
+//   background-color: $dark-grey;
+//   height: $input-height;
+//   border-radius: $input-br;
+//   border: none;
+//   box-shadow: $drop-shadow;
+//   color: $white;
+//   font-family: $font-input;
+//   outline-style: none;
+//   padding-left: 2.5em;
+//   padding-right: 2.5em;
+//   width: 100%;
+
+//   &:focus {
+//     box-shadow: $focus-glow-input;
+//   }
+
+//   &:disabled {
+//     background-color: $black;
+//     color: $dark-grey;
+//     box-shadow: none;
+//     border-color: $dark-grey;
+//     border-width: 1px;
+//     border-style: solid;
+//     cursor: auto;
+//   }
+// }
+</style>
--- src/components/TicketShop.vue
+++ src/components/TicketShop.vue
@@ -1,15 +1,22 @@
 <template>
   <div>
-    <Input class="input inputName" label="Name" />
+    <Input disabled="true" class="input inputName" label="Name" />
     <Input class="input inputEmail" label="Email" />
+    <div class="cb-cont">
+      <InputChckBox class="inputCb" label="Thursday" />
+      <InputChckBox class="inputCb" label="Friday" />
+      <InputChckBox class="inputCb" label="Saturday" />
+    </div>
   </div>
 </template>
 
 <script>
 import Input from './InputField'
+import InputChckBox from './InputCheckbox'
 export default {
   components: {
     Input,
+    InputChckBox,
   },
 }
 </script>
@@ -18,5 +25,11 @@ export default {
 .input {
   margin-left: 0;
   margin-right: 0;
+}
+
+.cb-cont {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
 }
 </style>
