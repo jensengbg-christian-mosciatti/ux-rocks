4f6c6f8be1ed2102ab017491ee24e17b7aaf143e
Frida Johansson <frida_mikaela@hotmail.com>
2020-11-05 09:32:00 +0100

    general scss links

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,9 +1,25 @@
 <template>
-  <div></div>
+  <div class="contact-container">
+    <h1>Contact</h1>
+    <p>
+      Before you contact us we would be happy if you first look in the FAQ and
+      look for your answer there. Contact us in the form or send an email to
+      info@uxrocks.com. We will get back to you as soon as we can.
+    </p>
+  </div>
 </template>
 
 <script>
 export default {}
 </script>
 
-<style></style>
+<style lang="scss" scoped>
+.contact-container {
+  h1 {
+    color: $white;
+  }
+  p {
+    color: $white;
+  }
+}
+</style>
