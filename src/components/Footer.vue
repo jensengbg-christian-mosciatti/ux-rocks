5cf5d858ba1f729c38a24136239fb2ded0b1a72a
jensengbg-christian-mosciatti <christian.mosciatti@gmail.com>
2020-11-08 16:34:42 +0100

    Footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -22,13 +22,13 @@
         <h2>GENERAL ENQUIRIES</h2>
         <p>
           Got a question? Please read through our FAQ’s carefully before
-          emailing us! info@uxrocks.com
+          emailing us! <a href="info@uxrocks.com">info@uxrocks.com</a>
         </p>
       </div>
       <div class="social-cont">
-        <img src="@/assets/spotify.svg" alt="Spotify logo" />
-        <img src="@/assets/insta.svg" alt="Instagram logo" />
-        <img src="@/assets/facebook.svg" alt="Facebook logo" />
+        <a href=""><img src="@/assets/spotify.svg" alt="Spotify logo"/></a>
+        <a href=""><img src="@/assets/insta.svg" alt="Instagram logo"/></a>
+        <a href=""><img src="@/assets/facebook.svg" alt="Facebook logo"/></a>
       </div>
     </section>
   </footer>
@@ -48,10 +48,10 @@ footer {
   // display: flex;
   // flex-direction: column;
   display: grid;
-  grid-template: 'links' 1fr 'general' 1fr 'tickets' 1fr / 1fr;
+  grid-template: 'links' 1fr 'tickets' 1fr 'general' 1fr / 1fr;
 
   width: 18em;
-  margin: auto;
+  margin: $margin-v-section auto 0 auto;
 }
 
 .linkz {
@@ -65,16 +65,24 @@ footer {
 .general {
   grid-area: general;
 }
+section {
+  margin: $margin-v-section/2 0;
+}
 
 section,
 section > div:nth-of-type(1) {
   display: flex;
   flex-direction: column;
+  justify-content: space-between;
   align-items: center;
 
-  margin: $margin-v-section/2 0;
   p {
     text-align: center;
+    font-size: 0.85em;
+    a {
+      font-family: inherit;
+      font-size: inherit;
+    }
   }
 }
 
@@ -84,11 +92,45 @@ section > div:nth-of-type(1) {
   width: 70%;
 }
 
-// @media only screen and (min-width: $desktop-query) {
-//   footer {
-//     width: 100%;
-//     flex-direction: row;
-//     max-width: $desktop-query;
-//   }
-// }
+a {
+  position: relative;
+}
+
+a::before {
+  content: '';
+  position: absolute;
+  width: 100%;
+  height: 1px;
+  bottom: 0;
+  left: 0;
+  background-color: $white;
+  visibility: hidden;
+  transform: scaleX(0);
+  transition: all 0.2s ease-in-out 0s;
+}
+
+a:hover::before {
+  visibility: visible;
+  transform: scaleX(1);
+}
+
+@media only screen and (min-width: $desktop-query) {
+  footer {
+    width: 100%;
+    grid-template: 'links general tickets' 1fr / 1fr 1fr 1fr;
+
+    max-width: $desktop-query;
+  }
+  .linkz {
+    align-items: flex-start;
+  }
+  .tickets,
+  section.tickets > div:nth-of-type(1) {
+    align-items: flex-end;
+
+    button {
+      margin: 0;
+    }
+  }
+}
 </style>
